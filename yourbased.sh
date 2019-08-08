63ac807c282f8ac6f8feaa5fabcf34c2f03b5763
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2019-08-08 15:08:22 -0400

    Update yourbased.sh

--- yourbased.sh
+++ yourbased.sh
@@ -4,10 +4,11 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update && apt-get install -y tzdata
 gem install bundler -v 2.0.1
 # before_install
-export PATH=$PWD/travis_phantomjs/phantomjs-2.1.1-linux-x86_64/bin:$PATH
-if [ $(phantomjs --version) != '2.1.1' ]; then rm -rf $PWD/travis_phantomjs; mkdir -p $PWD/travis_phantomjs; fi
-if [ $(phantomjs --version) != '2.1.1' ]; then wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2 -O $PWD/travis_phantomjs/phantomjs-2.1.1-linux-x86_64.tar.bz2; fi
-if [ $(phantomjs --version) != '2.1.1' ]; then tar -xvf $PWD/travis_phantomjs/phantomjs-2.1.1-linux-x86_64.tar.bz2 -C $PWD/travis_phantomjs; fi
+
+rm -rf $PWD/travis_phantomjs; mkdir -p $PWD/travis_phantomjs
+wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2 -O $PWD/travis_phantomjs/phantomjs-2.1.1-linux-x86_64.tar.bz2
+tar -xvf $PWD/travis_phantomjs/phantomjs-2.1.1-linux-x86_64.tar.bz2 -C $PWD/travis_phantomjs
+PATH=$PWD/travis_phantomjs/phantomjs-2.1.1-linux-x86_64/bin:$PATH
 phantomjs --version
 # install
 bundle install --jobs=3 --retry=3
